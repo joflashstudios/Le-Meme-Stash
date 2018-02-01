ce5e5b0117c7d529db7d8b6cf903a2bcf3d725bd
joflashstudios <joflashstudios@gmail.com>
2018-01-31 20:04:18 -0600

    got file reading working - JR

--- LeMemeStash/app/src/main/java/com/example/trinity/lememestash/MainActivity.java
+++ LeMemeStash/app/src/main/java/com/example/trinity/lememestash/MainActivity.java
@@ -1,52 +1,91 @@
 package com.example.trinity.lememestash;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_MEME_GET = 420;
+    private static final int REQUEST_READ_EXTERNAL_STORAGE = 666;
+
+    private GridView memeGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        memeGrid = findViewById(R.id.meme_grid);
+
+        fab.setOnClickListener(view -> {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
+            Intent open = new Intent(Intent.ACTION_GET_CONTENT);
+            open.setType("*/*");
+            if (open.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(open, REQUEST_MEME_GET);
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_MEME_GET && resultCode == RESULT_OK) {
+            Uri fullPhotoUri = data.getData();
+            handleMemeAddition(fullPhotoUri);
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void handleMemeAddition(Uri imageUri) {
+        @SuppressLint("StaticFieldLeak")
+        AsyncTask<Void, Void, Bitmap> task = new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... uris) {
+                try {
+                    return getBitmapFromUri(imageUri);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                Bitmap resized = ThumbnailUtils.extractThumbnail(result, 100, 100);
+                ((ImageView)findViewById(R.id.image1)).setImageBitmap(resized);
+            }
+        };
 
-        return super.onOptionsItemSelected(item);
+        task.execute();
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
     }
 }
